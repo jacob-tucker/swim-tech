bcb553ccfb316b71ad711829ea157415fc8051e9
jacob-tucker <jacobtucker818@gmail.com>
2022-05-30 00:42:32 -0500

    add section

--- pages/technologies.js
+++ pages/technologies.js
@@ -16,11 +16,11 @@ export default function Technologies() {
         </p>
         <img src="/goggles.jpg" alt="swimming goggles" />
         <p>
-          Although it may be super obvious, goggles and a swimming suit are needed to 
+          Although it may be super obvious, goggles and a swimming suit are needed to
           get in the water. Goggles are the piece of equipment you wear to keep water out
-          of your eyes, and picking the right ones are really important! Recreationally, 
-          picking goggles that keep the water out of your eyes is most important so you 
-          stay comfortable in the water. In competitive swimming however, you also need 
+          of your eyes, and picking the right ones are really important! Recreationally,
+          picking goggles that keep the water out of your eyes is most important so you
+          stay comfortable in the water. In competitive swimming however, you also need
           to worry about how tight the goggles are so you decrease water resistance.
         </p>
         <p>
@@ -33,9 +33,9 @@ export default function Technologies() {
         <p className="section-header">Common Technologies</p>
         <img src="/swimcap.jpg" width="400" alt="swimming cap" />
         <p>
-          Some technologies in swimming aren't essential, but come super in handy (whether it 
+          Some technologies in swimming aren't essential, but come super in handy (whether it
           be for practice, exercise, or racing) and are seen very often. One of those is a swim
-          cap. 
+          cap.
         </p>
         <p>
           A swim cap is something you wear over your head to keep your hair contained. The reasons for this
@@ -46,10 +46,23 @@ export default function Technologies() {
         </p>
         <p className="section-header">Assumptions in Technologies</p>
         <p>
-          Danielle Obe, a founding member of the Black swimming association, 
-          mentions even the technologies behind swimming (like caps) are designed for caucasian swimmers. 
-          Swim caps are not designed to fit the Afro hairstyle, which makes it very difficult for Black 
+          Danielle Obe, a founding member of the Black swimming association,
+          mentions even the technologies behind swimming (like caps) are designed for caucasian swimmers.
+          Swim caps are not designed to fit the Afro hairstyle, which makes it very difficult for Black
           swimmers to take advantage of the technology (Walker).
+        </p>
+        <img src="/swimcapsblack.png" width="400" alt="swim cap for Black people" /><br />
+        <p>
+          In 2021, the Olympics banned Soul Cap, a swim cap designed by a Black-owned company
+          for thick, curly hair from being used. "We hoped to further our work for diversity in
+          swimming by having our swim caps certified for competition, so swimmers at any level
+          don't have to choose between the sport they love and their hair," said Toks Ahmed and Michael Champman,
+          the co-founders of Soul Cap.
+        </p>
+        <p>
+          Unforutnately, technologies like these discourage young Black swimmers from participating
+          in the sport if they cannot see a future where they have to succumb to poor technologies
+          to even compete.
         </p>
         <p>
           This is worth bringing up because as we learn about technologies in the space,
@@ -67,7 +80,7 @@ export default function Technologies() {
         </p>
         <p>
           Kickboards are a little floatation device that allow you to put your arms on and keep
-          the front half of your body afloat. These are great for training your legs, because 
+          the front half of your body afloat. These are great for training your legs, because
           the only way to move forward is to kick.
         </p>
         <p>
@@ -88,7 +101,7 @@ export default function Technologies() {
         </p>
         <p className="section-header">Swim Wearables</p>
         <p>
-          There are quite a few technologies meant to help improve your stroke, but they are a bit 
+          There are quite a few technologies meant to help improve your stroke, but they are a bit
           more complex than a simple kickboard or flippers. The reason is because they require equipping
           something to your body to get a more deep understanding of how you're swimming.
         </p>
@@ -97,27 +110,27 @@ export default function Technologies() {
           <b>SMARTPADDLE</b>
         </p>
         <p>
-          "SmartPaddle" is one such sports wearable where you equip a sensor to your fingers while 
+          "SmartPaddle" is one such sports wearable where you equip a sensor to your fingers while
           you swim.
         </p>
         <p>
-          As mentioned on their <a href="https://simplifaster.com/articles/swim-faster-smartpaddle-sensor/" target="_blank" rel="noreferrer">website</a>, "SmartPaddle is a wearable sensor that measures the force, speed, and orientation of your 
-          swimming strokes. It shows whether the swimmer can apply the right amount of 
+          As mentioned on their <a href="https://simplifaster.com/articles/swim-faster-smartpaddle-sensor/" target="_blank" rel="noreferrer">website</a>, "SmartPaddle is a wearable sensor that measures the force, speed, and orientation of your
+          swimming strokes. It shows whether the swimmer can apply the right amount of
           force in the right direction at the right time."
         </p>
         <p>
-          More simply, the technology connects and sends information about the swimmer to a mobile 
+          More simply, the technology connects and sends information about the swimmer to a mobile
           device. That way, the swimmer doesn't have to worry about the device while they swim,
           but can look at the information after.
         </p>
         <p>
-          One interesting part of sports wearables with swimming if you have to make sure not to impact 
+          One interesting part of sports wearables with swimming if you have to make sure not to impact
           the hydrodynamics of the swimmer too heavily. Luckily, the SmartPaddle doesn't effect a swimmer's
           technique enough to make a big difference on the time.
         </p>
         <p className="section-header">Competitive Technologies</p>
         <p>
-          In the next lesson, we will talk about competitive swimming, and the 
+          In the next lesson, we will talk about competitive swimming, and the
           technologies behind it. To access this lesson, please return to the home
           page and select "Competitive Swimming".
         </p>
